303c3fa6c4ea111d24f75d257f1fd6b5021f8819
maxefimov <efimov.academy.top@gmail.com>
2023-04-20 19:02:07 +0300

    20-04-23

--- OopInheritenceProject/OopInheritenceProject.cpp
+++ OopInheritenceProject/OopInheritenceProject.cpp
@@ -1,6 +1,4 @@
-﻿// OopInheritanceProject.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 using namespace std;
@@ -24,6 +22,10 @@ public:
     string ToString()
     {
         return "Name: " + name + " (" + to_string(age) + ")";
+    }
+    ~Person()
+    {
+        cout << this << " Person destruct\n";
     }
 };
 ostream& operator<<(ostream& out, const Person& person)
@@ -49,7 +51,7 @@ class Student : public Person
 {
     string groupName;
 public:
-    Student(string name, int age) : Person(name, age) 
+    Student(string name, int age) : Person(name, age)
     {
         cout << this << " Student construct\n";
     }
@@ -67,6 +69,11 @@ public:
     {
         return Person::ToString() + " Group: " + groupName;
     }
+
+    ~Student()
+    {
+        cout << this << " Student destruct\n";
+    }
 };
 
 ostream& operator<<(ostream& out, const Student& student)
@@ -83,17 +90,8 @@ int main()
     student.GroupName() = "PV21";
     cout << student << "\n";*/
 
-    User user("Bob", 23);
+    //User user("Bob", 23);
+
+    Student student("Bob", 23);
 
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
